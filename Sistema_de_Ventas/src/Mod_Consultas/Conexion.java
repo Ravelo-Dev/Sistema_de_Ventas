5c051814fb5d3463741e60080dc7d0a09eb700ba
Mravelo <Mravelo@192.168.0.112>
2023-08-02 22:48:14 -0400

    MENSAJE DE ALERTA DE LA CONEXION AGREGADO

--- Sistema_de_Ventas/src/Mod_Consultas/Conexion.java
+++ Sistema_de_Ventas/src/Mod_Consultas/Conexion.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 public class Conexion {
 	Connection Con;
 	
@@ -11,9 +13,11 @@ public class Conexion {
 		try {
 			String MyDB = "jdbc:mysql://localhost:3306/sistema_de_ventas";
 			Con = DriverManager.getConnection(MyDB, "root", "");
+			JOptionPane.showMessageDialog(null, "Conexion exitosa");
 			return Con;
 		} catch (SQLException e) {
 			System.out.println(e.toString());
+			JOptionPane.showMessageDialog(null, "Conexion no establecida" + e.getMessage());
 		}
 		return null;
 	}
