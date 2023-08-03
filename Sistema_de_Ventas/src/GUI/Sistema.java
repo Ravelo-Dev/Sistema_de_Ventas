95fa7663a83da657c1b1a72277ec7132f489197e
Mravelo <Mravelo@192.168.0.112>
2023-08-02 22:35:08 -0400

    AJUSTES A VENTANA "NUEVA VENTA"

--- Sistema_de_Ventas/src/GUI/Sistema.java
+++ Sistema_de_Ventas/src/GUI/Sistema.java
@@ -237,98 +237,98 @@ public class Sistema extends JFrame {
 		TabbedPane_ventanas.setBounds(264, 219, 1020, 463);
 		Panel_main_sistema.add(TabbedPane_ventanas);
 		
-		JPanel panel = new JPanel();
-		TabbedPane_ventanas.addTab("", new ImageIcon(Sistema.class.getResource("/Img/carrito-de-compras.png")), panel, null);
-		panel.setLayout(null);
+		JPanel PanelVentana_NuevaVenta = new JPanel();
+		TabbedPane_ventanas.addTab("", new ImageIcon(Sistema.class.getResource("/Img/carrito-de-compras.png")), PanelVentana_NuevaVenta, null);
+		PanelVentana_NuevaVenta.setLayout(null);
 		
 		JLabel Lbl_Codigo = new JLabel("Codigo");
 		Lbl_Codigo.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_Codigo.setBounds(28, 23, 70, 19);
-		panel.add(Lbl_Codigo);
+		PanelVentana_NuevaVenta.add(Lbl_Codigo);
 		
 		Txt_Codigo = new JTextField();
 		Txt_Codigo.setBorder(null);
 		Txt_Codigo.setBounds(28, 48, 142, 33);
-		panel.add(Txt_Codigo);
+		PanelVentana_NuevaVenta.add(Txt_Codigo);
 		Txt_Codigo.setColumns(10);
 		
 		JSeparator separator_txtCodigo = new JSeparator();
 		separator_txtCodigo.setBackground(new Color(252, 176, 66));
 		separator_txtCodigo.setForeground(new Color(252, 176, 66));
 		separator_txtCodigo.setBounds(28, 81, 142, 13);
-		panel.add(separator_txtCodigo);
+		PanelVentana_NuevaVenta.add(separator_txtCodigo);
 		
 		JLabel Lbl_Descripcion = new JLabel("Descripcion");
 		Lbl_Descripcion.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_Descripcion.setBounds(213, 23, 91, 19);
-		panel.add(Lbl_Descripcion);
+		PanelVentana_NuevaVenta.add(Lbl_Descripcion);
 		
 		Txt_Descripcion = new JTextField();
 		Txt_Descripcion.setColumns(10);
 		Txt_Descripcion.setBorder(null);
 		Txt_Descripcion.setBounds(213, 48, 230, 33);
-		panel.add(Txt_Descripcion);
+		PanelVentana_NuevaVenta.add(Txt_Descripcion);
 		
 		JSeparator separator_txtDescripcion = new JSeparator();
 		separator_txtDescripcion.setForeground(new Color(252, 176, 66));
 		separator_txtDescripcion.setBackground(new Color(252, 176, 66));
 		separator_txtDescripcion.setBounds(213, 81, 230, 13);
-		panel.add(separator_txtDescripcion);
+		PanelVentana_NuevaVenta.add(separator_txtDescripcion);
 		
 		JLabel Lbl_Cantidad = new JLabel("Cantidad");
 		Lbl_Cantidad.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_Cantidad.setBounds(480, 23, 70, 19);
-		panel.add(Lbl_Cantidad);
+		PanelVentana_NuevaVenta.add(Lbl_Cantidad);
 		
 		Txt_Canidad = new JTextField();
 		Txt_Canidad.setColumns(10);
 		Txt_Canidad.setBorder(null);
 		Txt_Canidad.setBounds(480, 48, 98, 33);
-		panel.add(Txt_Canidad);
+		PanelVentana_NuevaVenta.add(Txt_Canidad);
 		
 		JSeparator separator_txtCantidad = new JSeparator();
 		separator_txtCantidad.setForeground(new Color(252, 176, 66));
 		separator_txtCantidad.setBackground(new Color(252, 176, 66));
 		separator_txtCantidad.setBounds(480, 81, 98, 13);
-		panel.add(separator_txtCantidad);
+		PanelVentana_NuevaVenta.add(separator_txtCantidad);
 		
 		Txt_Precio = new JTextField();
 		Txt_Precio.setColumns(10);
 		Txt_Precio.setBorder(null);
 		Txt_Precio.setBounds(611, 48, 101, 33);
-		panel.add(Txt_Precio);
+		PanelVentana_NuevaVenta.add(Txt_Precio);
 		
 		JSeparator separator_txtPrecio = new JSeparator();
 		separator_txtPrecio.setForeground(new Color(252, 176, 66));
 		separator_txtPrecio.setBackground(new Color(252, 176, 66));
 		separator_txtPrecio.setBounds(611, 81, 101, 13);
-		panel.add(separator_txtPrecio);
+		PanelVentana_NuevaVenta.add(separator_txtPrecio);
 		
 		JLabel Lbl_Precio = new JLabel("Precio");
 		Lbl_Precio.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_Precio.setBounds(611, 23, 70, 19);
-		panel.add(Lbl_Precio);
+		PanelVentana_NuevaVenta.add(Lbl_Precio);
 		
 		Txt_StockDisp = new JTextField();
 		Txt_StockDisp.setColumns(10);
 		Txt_StockDisp.setBorder(null);
 		Txt_StockDisp.setBounds(745, 48, 100, 33);
-		panel.add(Txt_StockDisp);
+		PanelVentana_NuevaVenta.add(Txt_StockDisp);
 		
 		JSeparator separator_txtStockDisp = new JSeparator();
 		separator_txtStockDisp.setForeground(new Color(252, 176, 66));
 		separator_txtStockDisp.setBackground(new Color(252, 176, 66));
 		separator_txtStockDisp.setBounds(745, 81, 100, 13);
-		panel.add(separator_txtStockDisp);
+		PanelVentana_NuevaVenta.add(separator_txtStockDisp);
 		
 		JLabel Lbl_StockDisp = new JLabel("Stock Disponible");
 		Lbl_StockDisp.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_StockDisp.setBounds(745, 23, 126, 19);
-		panel.add(Lbl_StockDisp);
+		PanelVentana_NuevaVenta.add(Lbl_StockDisp);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(28, 105, 953, 219);
-		panel.add(scrollPane);
+		PanelVentana_NuevaVenta.add(scrollPane);
 		
 		table = new JTable();
 		table.setModel(new DefaultTableModel(
@@ -344,56 +344,70 @@ public class Sistema extends JFrame {
 		JLabel Lbl_DNI_RUC = new JLabel("DNI/RUC");
 		Lbl_DNI_RUC.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_DNI_RUC.setBounds(28, 335, 70, 19);
-		panel.add(Lbl_DNI_RUC);
+		PanelVentana_NuevaVenta.add(Lbl_DNI_RUC);
 		
 		Txt_DNI_RUC = new JTextField();
 		Txt_DNI_RUC.setColumns(10);
 		Txt_DNI_RUC.setBorder(null);
 		Txt_DNI_RUC.setBounds(28, 360, 115, 33);
-		panel.add(Txt_DNI_RUC);
+		PanelVentana_NuevaVenta.add(Txt_DNI_RUC);
 		
 		JSeparator separator_txtDNI_RUC = new JSeparator();
 		separator_txtDNI_RUC.setForeground(new Color(252, 176, 66));
 		separator_txtDNI_RUC.setBackground(new Color(252, 176, 66));
 		separator_txtDNI_RUC.setBounds(28, 393, 115, 13);
-		panel.add(separator_txtDNI_RUC);
+		PanelVentana_NuevaVenta.add(separator_txtDNI_RUC);
 		
 		JLabel Lbl_Nombre = new JLabel("Nombre");
 		Lbl_Nombre.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_Nombre.setBounds(185, 335, 70, 19);
-		panel.add(Lbl_Nombre);
+		PanelVentana_NuevaVenta.add(Lbl_Nombre);
 		
 		Txt_Nombre = new JTextField();
 		Txt_Nombre.setColumns(10);
 		Txt_Nombre.setBorder(null);
 		Txt_Nombre.setBounds(185, 360, 218, 33);
-		panel.add(Txt_Nombre);
+		PanelVentana_NuevaVenta.add(Txt_Nombre);
 		
 		JSeparator separator_txtNombre = new JSeparator();
 		separator_txtNombre.setForeground(new Color(252, 176, 66));
 		separator_txtNombre.setBackground(new Color(252, 176, 66));
 		separator_txtNombre.setBounds(185, 393, 218, 13);
-		panel.add(separator_txtNombre);
+		PanelVentana_NuevaVenta.add(separator_txtNombre);
 		
 		JPanel Btn_Imprimir = new JPanel();
-		Btn_Imprimir.setBounds(433, 360, 115, 33);
-		panel.add(Btn_Imprimir);
+		Btn_Imprimir.setBounds(435, 360, 115, 33);
+		Btn_Imprimir.setBackground(new Color(252, 176, 66));
+		PanelVentana_NuevaVenta.add(Btn_Imprimir);
 		Btn_Imprimir.setLayout(null);
 		
 		JLabel Lbl_Btn_Imprimir = new JLabel("IMPRIMIR");
+		Lbl_Btn_Imprimir.setHorizontalAlignment(SwingConstants.CENTER);
 		Lbl_Btn_Imprimir.setFont(new Font("Roboto", Font.PLAIN, 16));
-		Lbl_Btn_Imprimir.setBounds(23, 0, 70, 33);
+		Lbl_Btn_Imprimir.setBounds(10, 0, 95, 33);
 		Btn_Imprimir.add(Lbl_Btn_Imprimir);
 		
 		JLabel Lbl_Total = new JLabel("Total a Pagar");
 		Lbl_Total.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_Total.setBounds(777, 365, 124, 19);
-		panel.add(Lbl_Total);
+		PanelVentana_NuevaVenta.add(Lbl_Total);
 		
 		JLabel Lbl_TotalMonto = new JLabel("- - - - ");
 		Lbl_TotalMonto.setFont(new Font("Roboto", Font.PLAIN, 16));
 		Lbl_TotalMonto.setBounds(911, 365, 70, 19);
-		panel.add(Lbl_TotalMonto);
+		PanelVentana_NuevaVenta.add(Lbl_TotalMonto);
+		
+		JPanel Btn_Eliminar = new JPanel();
+		Btn_Eliminar.setLayout(null);
+		Btn_Eliminar.setBackground(new Color(252, 176, 66));
+		Btn_Eliminar.setBounds(866, 48, 115, 33);
+		PanelVentana_NuevaVenta.add(Btn_Eliminar);
+		
+		JLabel Lbl_Btn_Eliminar = new JLabel("ELIMINAR");
+		Lbl_Btn_Eliminar.setHorizontalAlignment(SwingConstants.CENTER);
+		Lbl_Btn_Eliminar.setFont(new Font("Roboto", Font.PLAIN, 16));
+		Lbl_Btn_Eliminar.setBounds(10, 0, 95, 33);
+		Btn_Eliminar.add(Lbl_Btn_Eliminar);
 		
 		JPanel panel_1 = new JPanel();
 		TabbedPane_ventanas.addTab("", new ImageIcon(Sistema.class.getResource("/Img/atencion-al-cliente.png")), panel_1, null);
